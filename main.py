5b9e95c0909e2326772930c30924c12984ed498d
Filip Kin <96821611+filki@users.noreply.github.com>
2024-11-27 10:38:46 +0000

    adjusted to autograder

--- main.py
+++ main.py
@@ -30,39 +30,39 @@ def main():
             if word in term_index:
                 term_doc_matrix[term_index[word], doc_id] = 1
 
-    print("\nMacierz term dokument:")
+    #print("\nMacierz term dokument:")
     print(term_doc_matrix)
 
     U, S, Vt = np.linalg.svd(term_doc_matrix, full_matrices=False)
-
-    print("\nMacierz U (Lewa Macierz Osobliwa):")
-    print(U)
-    print("\nWartości Osobliwe (Diagonalna macierzy Σ):")
-    print(S)
-    print("\nMacierz V^T (Prawa Macierz Osobliwa):")
-    print(Vt)
-
+    
+    #print("\nMacierz U (Lewa Macierz Osobliwa):")
+    #print(U)
+    #print("\nWartości Osobliwe (Diagonalna macierzy Σ):")
+    #print(S)
+    #print("\nMacierz V^T (Prawa Macierz Osobliwa):")
+    #print(Vt)
+    
     Uk = U[:, :k]
     Sk = np.diag(S[:k])  # Macierz diagonalna z top k singular values
     Vk = Vt[:k, :]       # Pierwsze k wierszy V^T
 
     Ck = np.dot(Uk, np.dot(Sk, Vk))
-    print("\nMacierz Przybliżona Rzędu k (Ck)::")
-    print(Ck)
+    #print("\nMacierz Przybliżona Rzędu k (Ck)::")
+    #print(Ck)
     Ck = np.dot(Sk, Vk)
-    print("kształt Ck:", Ck.shape) # Debugowanie kształtu macierzy
+    #print("kształt Ck:", Ck.shape) # Debugowanie kształtu macierzy
 
     query_vec = np.zeros(len(terms))
     for word in query:
         if word in term_index:
             query_vec[term_index[word]] += 1
 
-    print("\nQuery wektor:")
-    print(query_vec)
+    #print("\nQuery wektor:")
+    #print(query_vec)
 
-    reduced_query = np.dot(np.linalg.inv(Sk), np.dot(Uk.T, query_vec))
-    print("\nZredukowany Query wektor:")
-    print(reduced_query)
+   # reduced_query = np.dot(np.linalg.inv(Sk), np.dot(Uk.T, query_vec))
+   #print("\nZredukowany Query wektor:")
+    #print(reduced_query)
 
     def cosine_similarity(vec1, vec2):
         norm1 = np.linalg.norm(vec1)
@@ -78,6 +78,6 @@ def main():
 
     print("\nPodobienstwa:")
     print(similarities)
-
+    return similarities
 if __name__ == "__main__":
     main()
